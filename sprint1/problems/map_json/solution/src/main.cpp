abce31a5a533cb9648928667a61439e6fe9c2b94
s.garnov <s.garnov@allmonitoring.ru>
2023-03-17 23:10:53 +0300

    bred

--- sprint1/problems/map_json/solution/src/main.cpp
+++ sprint1/problems/map_json/solution/src/main.cpp
@@ -63,6 +63,7 @@ int main(int argc, const char* argv[]) {
 
         // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
         std::cout << "Server has started..."sv << std::endl;
+		std::cout << "server started..."sv <<std::endl;
 
         // 6. Запускаем обработку асинхронных операций
         RunWorkers(std::max(1u, num_threads), [&ioc] {
