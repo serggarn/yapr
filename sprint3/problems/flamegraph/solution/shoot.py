91b66b109da0c227520e47acf5ecd1962d6f0819
s.garnov <s.garnov@allmonitoring.ru>
2023-05-05 19:48:43 +0300

    fix

--- sprint3/problems/flamegraph/solution/shoot.py
+++ sprint3/problems/flamegraph/solution/shoot.py
@@ -54,12 +54,15 @@ server = run(start_server())
 perf_record = run("perf record -F 100000 -p " + str(server.pid) + " -o perf.data -ag")
 #print("perf end")
 make_shots()
-stop(perf_record)
 stop(server)
 time.sleep(1)
+stop(perf_record)
+time.sleep(1)
+
 perf_script = subprocess.Popen(shlex.split("perf script -i perf.data"), stdout=subprocess.PIPE)
 stackcollapse = subprocess.Popen(shlex.split("./FlameGraph/stackcollapse-perf.pl"), stdin=perf_script.stdout, stdout = subprocess.PIPE)
 with open('graph.svg', "w") as outfile:
     flamegraps = subprocess.Popen(shlex.split("./FlameGraph/flamegraph.pl"), stdin=stackcollapse.stdout, stdout=outfile)
+    flamegraps.wait()
 
 print('Job done')
